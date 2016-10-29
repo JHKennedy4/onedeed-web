04171165d1ddec89682c04c1c8987a060d60f523
John Kennedy <jhkennedy4@gmail.com>
2016-10-29 03:20:58 -0700

    all tests passing after deletion

--- tests/integration/components/project-directory-test.js
+++ tests/integration/components/project-directory-test.js
@@ -12,13 +12,4 @@ test('it renders', function (assert) {
   this.render(hbs`{{project-directory}}`)
 
   assert.equal(this.$().text().trim(), '')
-
-  // Template block usage:
-  this.render(hbs`
-    {{#project-directory}}
-      template block text
-    {{/project-directory}}
-  `)
-
-  assert.equal(this.$().text().trim(), 'template block text')
 })
